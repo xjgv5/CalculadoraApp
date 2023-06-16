52f899a24371ff2171730701dca2dcf0d821e693
Javier Gomez <javier.gov@outlook.es>
2023-06-15 23:01:32 -0600

    [UPDATE] Ciclo agregado y manejo de errores

--- src/CalculadoraApp.java
+++ src/CalculadoraApp.java
@@ -8,50 +8,62 @@ public class CalculadoraApp {
         int opcion;
         int resultado;
 
-        System.out.println("---- Aplicacion Calculadora ----");
-        System.out.println("1. Suma");
-        System.out.println("2. Resta");
-        System.out.println("3. Multiplicacion");
-        System.out.println("4. Division ");
-        System.out.println("5. Salir");
-        System.out.println("¿Que operacion desea realizar?");
-        Scanner consola = new Scanner(System.in);
-        opcion = Integer.parseInt(consola.nextLine());
+        while (true){
+            System.out.println("---- Aplicacion Calculadora ----");
+            System.out.println("1. Suma");
+            System.out.println("2. Resta");
+            System.out.println("3. Multiplicacion");
+            System.out.println("4. Division ");
+            System.out.println("5. Salir");
+            System.out.println("¿Que operacion desea realizar?");
+            try{
+                Scanner consola = new Scanner(System.in);
+                opcion = Integer.parseInt(consola.nextLine());
 
-        if(opcion >=1 && opcion <= 4 ){
-            System.out.println("Proporciona el primer valor : ");
-            operando1 = Integer.parseInt(consola.nextLine());
-            System.out.println("Proporciona el segundo valor");
-            operando2 = Integer.parseInt(consola.nextLine());
-            switch (opcion){
-                case 1:
-                    resultado = operando1 + operando2;
-                    System.out.println("Resultado: " + resultado);
-                    break;
-                case 2:
+                if(opcion >=1 && opcion <= 4 ){
+                    System.out.println("Proporciona el primer valor : ");
+                    operando1 = Integer.parseInt(consola.nextLine());
+                    System.out.println("Proporciona el segundo valor");
+                    operando2 = Integer.parseInt(consola.nextLine());
+                    switch (opcion){
+                        case 1:
+                            resultado = operando1 + operando2;
+                            System.out.println("Resultado de la suma: " + resultado);
+                            break;
+                        case 2:
 
-                    resultado = operando1 - operando2;
-                    System.out.println("Resultado: " + resultado);
-                    break;
-                case 3:
+                            resultado = operando1 - operando2;
+                            System.out.println("Resultado de la resta: " + resultado);
+                            break;
+                        case 3:
+                            resultado = operando1 * operando2;
+                            System.out.println("Resultado de la multiplicacion : " + resultado);
+                            break;
+                        case 4:
+                            resultado = operando1 / operando2;
+                            System.out.println("Resultado de la division: " + resultado);
+                            break;
 
-                    resultado = operando1 * operando2;
-                    System.out.println("Resultado: " + resultado);
-                case 4:
-                    resultado = operando1 / operando2;
-                    System.out.println("Resultado: " + resultado);
+                        default:
+                            System.out.println("Opción no válida");
+                            break;
+                    }
+                }
+                else if (opcion  == 5) {
+                    System.out.println("Hasta pronto");
                     break;
-                default:
-                    System.out.println("Opción no válida");
-                    break;
+                }
+                else {
+                    System.out.println("Opción erronea");
+                }
+                System.out.println();
+
             }
-        }
-    else if (opcion  == 5) {
-            System.out.println("Hasta pronto");
-    }
-    else {
-            System.out.println("Opción erronea");
-        }
+            catch (Exception e){
+                System.out.println("Ocurrio un error: " + e.getMessage());
+            }
 
+
+    }
     }
 }
